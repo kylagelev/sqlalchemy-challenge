9aa61d352bf5f30eeb45f9d21df751b91d733276
Kyla <75227767+kylagelev@users.noreply.github.com>
2021-02-27 18:11:09 -0800

    Update climate_app.py
    attempt, can't open live server, so going to try that next and then have to fill in queries, this is to be done this evening...

--- climate_app.py
+++ climate_app.py
@@ -1,11 +1,30 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     print("Welcome")
-    return ("Welcome to the Climate App."
-    Available Routes:
+    return (f"Welcome to the Climate App."
+            "Available Routes:" 
+            <br/>
+            "/api/v1.0/precipitation"
+            <br/>
+            "/api/v1.0/stations"
+            <br/>
+            "/api/v1.0/tobs"
+            <br/>
+            "/api/v1.0/<start>"
+            <br/>
+            "/api/v1.0/<start>/<end>")
+
+
 
 
 @app.route('/api/v1.0/precipitation')
