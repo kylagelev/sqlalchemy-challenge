26ffe26d3c8495b209b6ddf9d5cd7b5b8d06b3a1
Kyla <75227767+kylagelev@users.noreply.github.com>
2021-02-27 23:00:02 -0800

    initial route
    completed process for precipitation

--- climate_app.py
+++ climate_app.py
@@ -6,6 +6,15 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
+
+database_path = 'Resources/hawaii.sqlite'
+engine = create_engine(f'sqlite:///{database_path}')
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+measurement = Base.classes.measurement
+station = Base.classes.station
+session = Session(engine)
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -19,31 +28,40 @@ def home():
             "/api/v1.0/<start><br/>"
             "/api/v1.0/<start>/<end><br/>")
 
-
-
-
 @app.route('/api/v1.0/precipitation')
 def precipitation():
+    session = Session(engine)
+    results = session.query(measurement.date, measurement.prcp).all()
 
-    return
+    prcp_dates = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        prcp_dates.append(prcp_dict)
+    return jsonify(prcp_dates)
 
 @app.route('/api/v1.0/stations')
 def stations():
+    session = Session(engine)
 
     return
 
 @app.route('/api/v1.0/tobs')
 def tob():
+    session = Session(engine)
 
     return
 
 @app.route('/api/v1.0/<start>') 
 def start():
+    session = Session(engine)
 
 
     return
 
 @app.route('/api/v1.0/<start>/<end>')
 def startend():
+    session = Session(engine)
 
     return
